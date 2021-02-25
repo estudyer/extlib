8f119d1e3785fc9556983f8186daf21d24ece8be
javascript-tp5文件上传压缩[拖拽排序] <15710062687@163.com>
2021-02-25 15:48:45 +0800

    Add files via upload

---
+++ src/Crons.php
@@ -0,0 +1,18 @@
+<?php
+namespace libraries;
+
+/**
+ * Class Crons
+ * @package libraries
+ */
+class Crons
+{
+    public function crons() {
+    }
+
+    public function edit() {}
+
+    public function enable() {}
+
+    public function generate() {}
+}
